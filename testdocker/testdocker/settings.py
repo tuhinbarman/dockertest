86fdd9e1b250131073d40d935ba15ec1eed12f6c
Tuhin Barman <tuhin_201900062@smit.smu.edu.in>
2025-08-09 17:55:10 +0530

    changed allowed hosts settings

--- testdocker/testdocker/settings.py
+++ testdocker/testdocker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)6yr(bt19a!0w(+96_zwddy*^ozwcmn=8mijx*jrd!_1!kdbuw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
